2c78b9a74385c824e69d9a4c047882c1522bbf1f
clarissa2307 <Marthaleo07@gmail.com>
2022-02-09 22:44:48 -0600

    reiniciar

--- src/index.js
+++ src/index.js
@@ -4,13 +4,14 @@ var vreproductor = document.getElementById("reproductor");
 var origen = vreproductor.getElementsByTagName("source")[0];
 var vbtnReproducir = document.getElementById("btnReproducir");
 var vbtnpausa = document.getElementById("btnPausa");
-var vbtnreDetener = document.getElementById("btnDetener");
+var vbtnDetener = document.getElementById("btnDetener");
 //origen.src = listaReproduccion[posicion];
 vreproductor.load();
 
 vbtnReproducir.addEventListener("click",reproducir);
 vbtnpausa.addEventListener("click",pausa);
-vreproductor.addEventListener("click",actualizarPanel);
+vbtnDetener.addEventListener("click",detener);
+
 
 function reproducir(){
     vreproductor.play();
@@ -19,3 +20,8 @@ function reproducir(){
 function pausa(){
     vreproductor.pause();
 }
+function detener(){
+    vreproductor.pause();
+    vreproductor.currentTime = 0;
+}
+
